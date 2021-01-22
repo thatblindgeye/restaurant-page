198c732ff47cde292581b47d8640ecfd0339d1b0
thatblindgeye <thatblindgeye@gmail.com>
2021-01-22 09:47:39 -0500

    Redo page theme and update CSS

--- src/index.js
+++ src/index.js
@@ -13,9 +13,9 @@ const siteSettings = (() => {
 })();
 
 const siteTheme = (() => {
-  const stylesheet = document.querySelector("#page-style");
   const themeContainer = document.querySelector(".theme-toggle-container");
   const themeToggle = document.querySelector(".theme-toggle-switch");
+  const footerImg = document.querySelector("footer img");
 
   const _loadTheme = () => {
     if (localStorage.getItem("current theme") === "light") {
@@ -38,12 +38,16 @@ const siteTheme = (() => {
     themeContainer.setAttribute("aria-checked", "false");
     document.documentElement.setAttribute("theme", "dark");
     themeToggle.style.left = "0";
+    footerImg.setAttribute("src", 
+        "images/github-logos/GitHub-Mark-Light-64px.png");
   };
 
   const _setLightTheme = () => {
     themeContainer.setAttribute("aria-checked", "true");
     document.documentElement.setAttribute("theme", "light");
     themeToggle.style.left = "16px";
+    footerImg.setAttribute("src", 
+        "images/github-logos/GitHub-Mark-64px.png");
   };
 
   themeContainer.addEventListener("click", _changeTheme);
